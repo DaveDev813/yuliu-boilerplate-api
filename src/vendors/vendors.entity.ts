91d8e5b9340958e0cb80d2d559c327d7ee78c70a
Jimmy Batuhan <jimmybatuhan@voncore.net>
2019-06-04 17:06:46 +0800

    tuloy ko sa bahay, puchu lang

--- src/vendors/vendors.entity.ts
+++ src/vendors/vendors.entity.ts
@@ -31,6 +31,12 @@ export class Vendors{
     @Column({ type : "longtext", nullable : false })
     address : string;
 
+    @Column({ type : "varchar", nullable: false })
+    city : string;
+
+    @Column({ type : "varchar", nullable: true })
+    province : string;
+
     @Column({type : 'text', nullable : false })
     business_type : string;
 
@@ -42,6 +48,97 @@ export class Vendors{
 
     @Column({type : 'datetime', nullable : true })
     last_transaction_date : string;
+
+    @Column({ type : 'datetime' })
+    last_date_updated : string;
+
+    @Column({type : 'datetime', default : () => "CURRENT_TIMESTAMP" })
+    date_created : string;
+}
+
+@Entity()
+export class Vendor_Business_Address{
+    
+    @PrimaryGeneratedColumn() 
+    id: number;
+
+    @Column({ type : "text", nullable  : false }) 
+    branch_code : string;
+
+    @Column({ type : "text", nullable  : false }) 
+    contact_person : string;
+
+    @Column({ type : "varchar", nullable: true })
+    mobile_no : string;
+
+    @Column({ type : "varchar", nullable : true })
+    telephone_no : string;
+
+    @Column({type : 'text', nullable : true })
+    business_hours : string;
+
+    @Column({ type : "longtext", nullable : false })
+    address : string;
+
+    @Column({ type : "varchar", nullable: false })
+    city : string;
+
+    @Column({ type : "varchar", nullable: true })
+    province : string;
+
+    @Column({type : 'datetime', nullable : true })
+    last_transaction_date : string;
+
+    @Column({ type : 'datetime' })
+    last_date_updated : string;
+
+    @Column({type : 'datetime', default : () => "CURRENT_TIMESTAMP" })
+    date_created : string;
+}
+
+@Entity()
+export class Vendor_Employee{
+
+    @PrimaryGeneratedColumn() 
+    id: number;
+
+    @Column({ type : "text", nullable  : true }) 
+    employee_id : string;
+
+    @Column({ type : "text", nullable  : true }) 
+    vendor_id : string;
+
+    /** Vendor_business_address */
+    @Column({ type : "text", nullable  : true }) 
+    branch_code : string;
+
+    @Column({ type : "text", nullable  : true }) 
+    firstname : string;
+
+    @Column({ type : "text", nullable  : true }) 
+    middlename : string;
+
+    @Column({ type : "text", nullable  : true }) 
+    lastname : string;
+
+    @Column({ type : "text", nullable  : true }) 
+    position : string;
+
+    @Column({type : 'datetime', nullable : true })
+    last_transaction_date : string;
+    
+    @Column({ type : 'datetime' })
+    last_date_updated : string;
+
+    @Column({type : 'datetime', default : () => "CURRENT_TIMESTAMP" })
+    date_created : string;
+}
+
+@Entity()
+export class Vendor_Services{
+
+    @PrimaryGeneratedColumn() 
+    id: number;
 
     @Column({ type : 'datetime' })
     last_date_updated : string;
