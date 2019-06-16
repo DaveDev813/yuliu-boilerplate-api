2f9d9e65c7cd1734b9ee373e9dad4497ccfac1b0
aldrin027 <aldrin.gauran@gmail.com>
2019-06-16 22:23:17 +0800

    Fix Branches API

--- src/vendors/branches.controller.ts
+++ src/vendors/branches.controller.ts
@@ -1,88 +1,104 @@
-import { Controller, Post, Param, Body, Put, BadRequestException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Param,
+  Body,
+  Put,
+  BadRequestException,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
 import { searchDto } from 'src/_commons/commons.dto';
-import { newVendorBranchDto, updateVendorBranchDto } from './dto/branches.dto';
+import { NewVendorBranchDto, UpdateVendorBranchDto } from './dto/branches.dto';
 import { BranchesService } from './services/branches.service';
 import moment = require('moment');
 import voucherCodeGenerator = require('voucher-code-generator');
-import { ApiUseTags } from '@nestjs/swagger';
+import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger';
 import { VendorsService } from './services/vendors.service';
 import faker = require('faker');
 import _ = require('lodash');
+import { AuthGuard } from '@nestjs/passport';
 
-@ApiUseTags(`Vendors Branches`)
+@ApiUseTags('Vendors Branches')
+@ApiBearerAuth()
+@UseGuards(AuthGuard())
 @Controller('vendor/branches')
-export class BranchesController{
+export class BranchesController {
+  constructor(
+    private readonly vendorService: VendorsService,
+    private readonly branchesService: BranchesService,
+  ) {}
 
-    constructor(
-        private readonly vendorService : VendorsService,
-        private readonly branchesService : BranchesService){
+  @Get(':id')
+  async getBranchInfo(@Param('id') branchId: number) {
+    /**
+     * @TODO
+     * INCLUDE THE VENDOR INFORMATION IN THE PAYLOAD
+     */
+
+    return await this.branchesService.getBranchById(branchId);
+  }
+
+  @Post('create')
+  async createBranch(@Body() branch: NewVendorBranchDto) {
+    const vendor = await this.vendorService.getVendorById(branch.vendorId);
+
+    if (!vendor) {
+      throw new BadRequestException('Invalid Vendor ID..');
     }
 
-    @Post(`create`)
-    async createBranch(@Body() branch : newVendorBranchDto){   
-
-        const vendor = await this.vendorService.getVendorById(branch.vendor_id);
-
-        if(vendor){
-            throw new BadRequestException('Invalid Vendor ID..');
-        }
-
-        if(vendor.vendor_status === `Disabled`){
-            throw new BadRequestException('Vendor is not active..');
-        }
-
-        const _result = await this.branchesService.createBranch(branch);
-
-        return { payload : _result.generatedMaps, raw : _result.raw };
+    if (vendor.vendor_status === `Disabled`) {
+      throw new BadRequestException('Vendor is not active..');
     }
 
-    @Post(`create/faker`)
-    async createFakeBraches(){
+    return await this.branchesService.createBranch(branch);
 
-        _.times( 100, async () => {
+    // return { payload: result.generatedMaps, raw: result.raw };
+  }
 
-            const fakeBranch = {
-                vendor_id : faker.random.number(),
-                branch_code : voucherCodeGenerator.generate({ length : 5, count : 1, pattern : `#####`, prefix : `BRANCH-` })[0],
-                contact_person : faker.name.findName(),
-                mobile_no : faker.phone.phoneNumberFormat(),
-                telephone_no : faker.phone.phoneNumberFormat(),
-                days_open : ['Mon','Wed','Tue','Fri'],
-                open_hours : "21:00",
-                address :`${faker.address.streetAddress()} ${faker.address.streetName()} ${faker.address.zipCode()}`,
-                city : faker.address.city(),
-                created_by : 1
-            };
+  @Post('create/faker')
+  async createFakeBraches() {
+    _.times(100, async () => {
+      const fakeBranch = {
+        vendorId: faker.random.number(),
+        branchCode: voucherCodeGenerator.generate({
+          length: 5,
+          count: 1,
+          pattern: `#####`,
+          prefix: `BRANCH-`,
+        })[0],
+        contactPerson: faker.name.findName(),
+        mobileNo: faker.phone.phoneNumberFormat(),
+        telephoneNo: faker.phone.phoneNumberFormat(),
+        daysOpen: ['Mon', 'Wed', 'Tue', 'Fri'],
+        openHours: '08:00',
+        closedHours: '20:00',
+        address: `${faker.address.streetAddress()} ${faker.address.streetName()} ${faker.address.zipCode()}`,
+        city: faker.address.city(),
+        branchStatus: 'Enabled',
+      };
 
-            await this.branchesService.createBranch(fakeBranch);
-        });
-    }    
+      await this.branchesService.createBranch(fakeBranch);
+    });
+  }
 
-    @Put(`update/:id`)
-    async updateBranch(@Param(`id`) branchId : number, @Body() revisions : updateVendorBranchDto){
+  @Put('update/:id')
+  async updateBranch(
+    @Param('id') branchId: number,
+    @Body() revisions: UpdateVendorBranchDto,
+  ) {
+    const vendor = await this.vendorService.getVendorById(revisions.vendorId);
 
-        if(revisions.vendor_id){
-
-            const vendor = await this.vendorService.getVendorById(revisions.vendor_id);
-
-            if(vendor){ throw new BadRequestException('Invalid Vendor ID..'); }
-
-            if(vendor.vendor_status === `Disabled`){ throw new BadRequestException('Vendor is not active..'); }
-        }
-
-        const _vendor = await this.branchesService.updateVendorBranch(branchId, revisions);
-
-        return { payload : _vendor.generatedMaps, raw : _vendor.raw };
+    if (!vendor) {
+      throw new BadRequestException('Invalid Vendor ID..');
     }
 
-    @Post(`branch/:id`)
-    async getBranchInfo(@Param(`id`) branchId : number){
-
-        /**
-         * @TODO 
-         * INCLUDE THE VENDOR INFORMATION IN THE PAYLOAD
-         */
-
-        return await this.branchesService.getBranchById(branchId);
+    if (vendor.vendor_status === 'Disabled') {
+      throw new BadRequestException('Vendor is not active..');
     }
+
+    return await this.branchesService.updateVendorBranch(branchId, revisions);
+
+    // return { payload: vendor.generatedMaps, raw: vendor.raw };
+  }
 }
